bb1ffc5894ce841864e2606362186b4a1d1697df
Ojas Brid <ojasbc21@iitk.ac.in>
2023-09-06 23:53:16 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: '../build'
+  },
   plugins: [react()],
-  base: "https://home.iitk.ac.in/~ojasbc21/"
+  base: "/Personal-Portfolio/"
 })
